a3c433336d35dfcc03abd9320b5badf5bee61efa
Sinem Korkmaz <SinemKrkmz@users.noreply.github.com>
2017-05-16 22:59:34 +0300

    Update user.py

--- app/test/user.py
+++ app/test/user.py
@@ -1,4 +1,8 @@
-from config import DefaultConfig
-import pytest
+import unittest
 
-
+class TestA(unittest.TestCase):
+    def test_abc(self):
+        a = 9
+        b = 3
+        c = a * b
+        self.assertEqual(c, 13, "nonono")
